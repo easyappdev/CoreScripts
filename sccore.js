173b1b28078711f88cef299ebefa1b2b68233e35
easyappdev <51762205+easyappdev@users.noreply.github.com>
2020-11-19 10:38:06 -0300

    Add files via upload
    Primer commit

---
+++ sccore.js
@@ -0,0 +1,34 @@
+var funList = []
+var enabledSites = [
+    "86ac3665-dc73-4926-92cd-de7463a086f6",
+    "ba26ad9e-a9f9-4307-a82c-dc86b0a8a172"
+]
+
+function callScript(_scName, _scData, _scFunHandle){
+    if(enabledSites.indexOf($('#hidWSId').val()) !== -1){
+        var handleData = {
+            scName: _scName,
+            scFunHandle: _scFunHandle
+        }
+        if(funList.length) {
+            let idx = funList.map(function(e) { 
+                return e.scName; 
+            }).indexOf(_scName);
+            if(idx === -1) {
+                funList.push(handleData);
+            }
+        }
+        else {
+            funList.push(handleData);
+        }
+        funScript(_scName, JSON.stringify(_scData));
+    }   
+}
+
+function funScriptSuccess(a) { 
+    var d = JSON.parse(a);
+    let idx = funList.map(function(e) { 
+        return e.scName; 
+    }).indexOf(d.scName);
+    window[funList[idx].scFunHandle](d.data);
+}
