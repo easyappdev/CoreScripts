7a91e20286ce3c3edcdb81e69a95bfb364fd5a6a
easyappdev <51762205+easyappdev@users.noreply.github.com>
2022-11-09 13:50:16 -0300

    Update sccore.js

--- sccore.js
+++ sccore.js
@@ -3,7 +3,7 @@ var enabledSites = [
     "98295549-F0D3-4168-9BEF-8342E7EA167B", //sitio demo1
     "5421A861-5BB3-41AA-A21E-5F4F9D8A7A33",  //sitio demo2 
     "2970FBBA-1147-49F4-82ED-81DE2BF5B2C1",  //sitio GremioTech
-    "5f6fb4fd-fc16-4a24-9d7d-291e9ee53282" //sitio Adma
+    "5F6FB4FD-FC16-4A24-9D7D-291E9EE53282" //sitio Adma
 ]
 
 function callScript(_scName, _scData, _scFunHandle){
