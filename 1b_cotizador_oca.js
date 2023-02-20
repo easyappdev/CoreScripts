838cffe6565a301b05816ca76a0ea137d8ae18c9
easyappdev <51762205+easyappdev@users.noreply.github.com>
2023-02-20 14:50:24 -0300

    Update 1b_cotizador_oca.js

--- 1b_cotizador_oca.js
+++ 1b_cotizador_oca.js
@@ -1,33 +1,29 @@
-// V1.0.38
+// V1.0.40
 // https://1bit.com.ar
 
 function CotizadorOCA_Tarifar_Envio_Corporativo(_Operativa, _CodigoPostalOrigen, _CodigoPostalDestino, _PesoTotal, _VolumenTotal, _CantidadPaquetes, _handleFunctionOK, _handleFunctionFail) {
+  var myHeaders = new Headers();
+  myHeaders.append("WSId", $('#hidWSId').val().toUpperCase());
+  myHeaders.append("Content-Type", "application/json");
 
-    var myHeaders = new Headers();
-    myHeaders.append("WSId", $('#hidWSId').val().toUpperCase());
-    myHeaders.append("Content-Type", "application/json");
+  var raw = JSON.stringify({
+	"Operativa": _Operativa,
+	"CodigoPostalOrigen": _CodigoPostalOrigen,
+	"CodigoPostalDestino": _CodigoPostalDestino,
+	"PesoTotal": _PesoTotal,
+	"VolumenTotal": _VolumenTotal,
+	"CantidadPaquetes": _CantidadPaquetes
+  });
+  
+  var requestOptions = {
+	method: 'POST',
+	headers: myHeaders,
+	body: raw,
+	redirect: 'follow'
+  };
 
-    var raw = JSON.stringify({
-        "Operativa": _Operativa,
-        "CodigoPostalOrigen": _CodigoPostalOrigen,
-        "CodigoPostalDestino": _CodigoPostalDestino,
-        "PesoTotal": _PesoTotal,
-        "VolumenTotal": _VolumenTotal,
-        "CantidadPaquetes": _CantidadPaquetes
-    });
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    fetch("https://apitools.1bitservices.com.ar/oca/TarifarEnvioCorporativo", requestOptions)
-        .then(response => response.text())
-        .then(result => function () { window[_handleFunctionOK](result) })
-        .catch(error => function() {
-            console.log("CotizadorOCA Error: ", error);
-            window[_handleFunctionFail](error);
-        });
-}
+  fetch("https://apitools.1bitservices.com.ar/oca/TarifarEnvioCorporativo", requestOptions)
+	.then(response => response.text())
+	.then(result => window[_handleFunctionOK](result))
+	.catch(error => window[_handleFunctionFail](error));
+};
